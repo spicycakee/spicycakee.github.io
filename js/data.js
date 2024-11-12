1e7dcd0b4222fcc504612e0c62bb356700bc063f
Kamille Galicia <161795888+spicycakee@users.noreply.github.com>
2024-11-12 23:05:26 +0800

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,32 @@
+const data = {
+    portfolios: [
+        {
+            techStack: "HTML, CSS, JS, PHP",
+            title: "Grandeur Hotel",
+            githubLink: "https://github.com/spicycakee/Grandeur-Hotel",
+            projectLink: "https://spicycakee.github.io/Grandeur-Hotel/",
+            imgLink: "images/portfolio/grandeur.jpg"
+        },
+        {
+            techStack: "HTML, CSS, JS",
+            title: "Contrast Agency Business",
+            githubLink: "https://github.com/spicycakee/Contrast",
+            projectLink: "https://spicycakee.github.io/Contrast/",
+            imgLink: "images/portfolio/project.jpg"
+        },
+        {
+            techStack: "HTML, CSS, JS",
+            title: "Nova Finance",
+            githubLink: "https://github.com/spicycakee/Nova-Finance",
+            projectLink: "https://spicycakee.github.io/Nova-Finance/",
+            imgLink: "images/portfolio/project2.jpg"
+        },
+        {
+            techStack: "HTML, CSS, JS",
+            title: "Zenith App Services",
+            githubLink: "https://github.com/spicycakee/Zenith-App",
+            projectLink: "https://spicycakee.github.io/Zenith-App/",
+            imgLink: "images/portfolio/project4.jpg"
+        }
+    ]
+}
